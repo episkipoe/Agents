6819d89912a015c199f3a28293bf25deaf891d38
dbennett <dbennett>
2008-10-01 02:17:25 +0000

    add turn genes

---
+++ genes/add_turn.cpp
@@ -0,0 +1,71 @@
+//add_turn
+#include <string>
+#include <stdio.h>
+#include <sstream>
+#include <common/shmem.h>
+#include <common/randomhelp.h>
+#include <nnets/nnets.h>
+int main(int argc, char *argv[]) {
+	srandom(100);
+	if(argc<2) return 0;
+	Net myNet;
+	//printf("add_movement started with file %s port %s\n", argv[0], argv[1]);
+	char * genome_file = argv[0];
+	int port = atoi(argv[1]);
+
+	int num_inputs = 10;
+	Neuron ** input_neurons = new Neuron*[num_inputs];
+	int num_hidden = 4;
+	Neuron ** hidden_neurons = new Neuron*[num_hidden];
+	int num_outputs = 3;
+	Neuron ** output_neurons = new Neuron*[num_outputs];
+	int num_geneneurons = 1;
+	Neuron ** gene_neurons = new Neuron*[num_geneneurons];
+
+
+	char input_name[] = "eye";
+	char hidden_name[] = "visual";
+	char output_name[] = "legs";
+	char gene_name[] = "//turn";
+	stringstream net_file;  net_file<<genome_file<<"."<<port<<".net";
+	myNet.loadFromFile(net_file.str().c_str());
+
+	for(int h = 0 ; h < num_hidden; h++) {
+		hidden_neurons[h] = myNet.addNeuron(HIDDEN, hidden_name);
+	}
+
+	for(int i = 0 ; i < num_inputs; i++) {
+		input_neurons[i] = myNet.addNeuron(INPUT, input_name);
+
+		for(int h = 0 ; h < num_hidden; h++) {
+			float weight = randomFloat(-1.5,1.5);
+			myNet.addSynapse(input_neurons[i], hidden_neurons[h], weight);
+		}
+	}
+
+	for(int o = 0 ; o < num_outputs; o++) {
+		output_neurons[o] = myNet.addNeuron(OUTPUT, output_name);
+
+		for(int h = 0 ; h < num_hidden; h++) {
+			float weight = randomFloat(-2.0,2.0);
+			myNet.addSynapse(hidden_neurons[h], output_neurons[o], weight);
+		}
+	}
+	for(int o = 0 ; o < num_geneneurons; o++) {
+		gene_neurons[o] = myNet.addNeuron(OUTPUT, gene_name);
+
+		for(int h = 0 ; h < num_hidden; h++) {
+			float weight = randomFloat(-1.5,1.5);
+			myNet.addSynapse(hidden_neurons[h], gene_neurons[o], weight);
+		}
+	}
+
+	delete [] input_neurons;
+	delete [] hidden_neurons;
+	delete [] output_neurons;
+	delete [] gene_neurons;
+	myNet.saveToFile(net_file.str().c_str());
+}
+
+//end
+
--- genes/turn.cpp
+++ genes/turn.cpp
@@ -1,16 +1,15 @@
-//move
+//turn
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <messages/message.h>
 int main(int argc, char *argv[]) {
 	int myPort = atoi(argv[0]);
-	char data[3]={0,0,0};
-	if(argc>1&&strlen(argv[1])>1) {
+	char data[2]={0,0};
+	if(argc>1&&strlen(argv[1])>0) {
 		data[0]=argv[1][0];
-		data[1]=argv[1][1];
 	}
-	send_message(myPort, 49152, MOVE, 2, data);
+	send_message(myPort, 49152, TURN, 1, data);
 	return 0;
 }
 
